a5584ff501861c12eb1d4f32bad3a50ce80c2312
Alesia - Imali SuMaku Kute <73201769+AlesiaLRuffin@users.noreply.github.com>
2020-11-20 23:29:21 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week4Homeworkpt2
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+            //alphabet
+        {
+for (int i = 0; i<26; i++)
+            {
+                Console.Write(Convert.ToChar(i + (int)'a') + "");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
